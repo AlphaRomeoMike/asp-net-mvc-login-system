59d690f6aac9572ac200653fd7fda8e9146c9cb2
Abdul Rafay Modi <subjectalpha32@gmail.com>
2021-04-17 13:20:49 +0500

    Add project files.

---
+++ Login System/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Login_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginEntities db = new LoginEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User u)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.Users.Add(u);
+                int a = db.SaveChanges();
+                if(a > 0)
+                {
+                    TempData["Signup"] = "<p class=\"alert alert-success\">User registered successfully</p>";
+                    ModelState.Clear();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Signup"] = "<p class=\"alert alert-danger\">User registration failed</p>";
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
---
+++ Login System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Login_System.MvcApplication" Language="C#" %>
