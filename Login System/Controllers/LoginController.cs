0c5e7e15f3efe3ef96c25b81243cb9944780460e
Abdul Rafay Modi <subjectalpha32@gmail.com>
2021-04-17 13:56:46 +0500

    Login not working properly :exclaimation:

--- Login System/Controllers/LoginController.cs
+++ Login System/Controllers/LoginController.cs
@@ -18,7 +18,21 @@ namespace Login_System.Controllers
         [HttpPost]
         public ActionResult Index(User u)
         {
-            return View();
+            var user = db.Users.Where(model => model.Email == u.Email && model.Password == u.Password).FirstOrDefault();
+
+            if(user != null)
+            {
+                Session["UserId"] = u.Id.ToString();
+                //Session["UserName"] = u.Firstname.ToString();
+                Session["User"] = u.Id.ToString();
+                TempData["Login"] = "<p class=\"alert alert-success\">Login successful</p>";
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                ViewBag.Error = "<p class=\"alert alert-danger\">Login failed! Pelase try again</p>";
+                return View();
+            }
         }
         [HttpPost]
         public ActionResult Signup(User u)
